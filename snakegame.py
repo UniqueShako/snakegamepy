d02a875f99163c807823280352408d8f49f794c4
Taylor <41836534+UniqueShako@users.noreply.github.com>
2022-10-31 08:18:02 -0600

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from collections import deque
 
 
 pygame.init()
@@ -17,15 +18,15 @@ game_over = False
 
 clock = pygame.time.Clock()
 
-prevx = [0]
-prevy = [0]
 xlist = [400]
 ylist = [300]
+savex = deque([])
+savey = deque([])
 foodx = round(random.randint(0,790)/10)*10
 foody = round(random.randint(0,590)/10)*10
 xchange = 0
 ychange = 0
-snakesize = 1
+snakesize = 0
 
 
 while not game_over:
@@ -47,25 +48,34 @@ while not game_over:
                 ychange = 10
             
     screen.fill(black)
+            
 
-    pygame.draw.rect(screen, blue, [xlist[0], ylist[0], 10,10])
-    xlist[0] += xchange
-    ylist[0] += ychange
-    playercoord = (xlist[0], ylist[0])
-    print(playercoord)
+
     pygame.draw.rect(screen, red, [foodx, foody, 10,10])
     if xlist[0] == foodx and ylist[0] == foody:
-        screen.fill(black)
         foodx = round(random.randint(0,790)/10)*10
         foody = round(random.randint(0,590)/10)*10
         pygame.draw.rect(screen, red, [foodx, foody, 10, 10])
         snakesize += 1
+        #xlist.append([0])
+        #ylist.append([0])
+        savex.append(0)
+        savey.append(0)
+    if snakesize > 0:
+        savex.pop()
+        savey.pop()
+        savex.append(xlist[0])
+        savey.append(ylist[0])
+        savex.rotate(1)
+        savey.rotate(1)
+    for piece in range(snakesize):
+        pygame.draw.rect(screen, blue, [savex[piece], savey[piece], 10, 10])
+    xlist[0] += xchange
+    ylist[0] += ychange
+    pygame.draw.rect(screen, blue, [xlist[0], ylist[0], 10,10])
     pygame.display.update()
-    clock.tick(20)
-
-    
-    
-
+    print(savex, savey)
+    clock.tick(15)
 
 pygame.quit()
 quit()
