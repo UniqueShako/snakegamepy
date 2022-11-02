61a73d0437d12fc120be821ecd2ec415b984b3d6
Taylor <41836534+UniqueShako@users.noreply.github.com>
2022-11-01 21:29:52 -0600

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -9,7 +9,10 @@ blue = (0,0,255)
 red = (255,0,0)
 black = (0,0,0)
 
-screen=pygame.display.set_mode((800,600))
+screenx = 800
+screeny = 600
+
+screen=pygame.display.set_mode((screenx,screeny))
 pygame.display.set_caption("Snake Game")
 
 
@@ -18,10 +21,12 @@ game_over = False
 
 clock = pygame.time.Clock()
 
-xlist = [400]
-ylist = [300]
+xpos = 400
+ypos = 300
+playercoords = (xpos, ypos)
 savex = deque([])
 savey = deque([])
+savecoords = []
 foodx = round(random.randint(0,790)/10)*10
 foody = round(random.randint(0,590)/10)*10
 xchange = 0
@@ -35,47 +40,60 @@ while not game_over:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                xchange = 10
-                ychange = 0
+                if xchange != -10:
+                    xchange = 10
+                    ychange = 0
             if event.key == pygame.K_LEFT:
-                xchange = -10
-                ychange = 0
+                if xchange != 10:
+                    xchange = -10
+                    ychange = 0
             if event.key == pygame.K_UP:
-                xchange = 0
-                ychange = -10
+                if ychange != 10:
+                    xchange = 0
+                    ychange = -10
             if event.key == pygame.K_DOWN:
-                xchange = 0
-                ychange = 10
+                if ychange != -10:
+                    xchange = 0
+                    ychange = 10
             
     screen.fill(black)
             
 
 
     pygame.draw.rect(screen, red, [foodx, foody, 10,10])
-    if xlist[0] == foodx and ylist[0] == foody:
+    if xpos == foodx and ypos == foody:
         foodx = round(random.randint(0,790)/10)*10
         foody = round(random.randint(0,590)/10)*10
         pygame.draw.rect(screen, red, [foodx, foody, 10, 10])
         snakesize += 1
-        #xlist.append([0])
-        #ylist.append([0])
         savex.append(0)
         savey.append(0)
     if snakesize > 0:
         savex.pop()
         savey.pop()
-        savex.append(xlist[0])
-        savey.append(ylist[0])
+        savex.append(xpos)
+        savey.append(ypos)
         savex.rotate(1)
         savey.rotate(1)
     for piece in range(snakesize):
         pygame.draw.rect(screen, blue, [savex[piece], savey[piece], 10, 10])
-    xlist[0] += xchange
-    ylist[0] += ychange
-    pygame.draw.rect(screen, blue, [xlist[0], ylist[0], 10,10])
+    xpos += xchange
+    ypos += ychange
+    playercoords = (xpos, ypos)
+    pygame.draw.rect(screen, blue, [xpos, ypos, 10,10])
     pygame.display.update()
-    print(savex, savey)
-    clock.tick(15)
+    savecoords = [(savex[i], savey[i]) for i in range(0, len(savex))]
+    for coord in savecoords:
+        if playercoords == coord:
+            game_over = True
+            print(coord)
+            print("Game Over!")
+    if xpos >= screenx or xpos < 0 or ypos >= screeny or ypos < 0:
+        game_over = True
+        print("Game Over!")
+    #print(savex, savey)
+    print(playercoords)
+    clock.tick(20)
 
 pygame.quit()
 quit()
